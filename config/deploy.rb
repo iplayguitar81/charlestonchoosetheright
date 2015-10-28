71477d97418f89f5720f59f78ba6541e7dc0d947
iplayguitar81 <iplayguitar81@yahoo.com>
2015-10-28 02:58:41 -0700

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,7 +43,7 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
-      #   execute :rake, 'cache:clear'
+         execute :rake, 'db:migrate'
       # end
     end
   end
