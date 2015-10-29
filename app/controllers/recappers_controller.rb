a2ee50a0c0f6137692b89ec969d284da36776bb8
iplayguitar81 <iplayguitar81@yahoo.com>
2015-10-29 03:22:05 -0700

    update

--- app/controllers/recappers_controller.rb
+++ app/controllers/recappers_controller.rb
@@ -1,6 +1,6 @@
 class RecappersController < ApplicationController
   before_action :set_recapper, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user!, except: [ :index, :show ]
 
   # GET /recappers
   # GET /recappers.json
