145f93dfc0553d0211cdbb5524fd1cec89f6b1f3
iplayguitar81 <iplayguitar81@yahoo.com>
2015-10-31 01:58:23 -0700

    update

--- db/migrate/20151031081503_add_attachment_image_to_stories.rb
+++ db/migrate/20151031081503_add_attachment_image_to_stories.rb
@@ -1,7 +1,10 @@
 class AddAttachmentImageToStories < ActiveRecord::Migration
+
+  include Paperclip::Schema
+
   def self.up
     change_table :stories do |t|
-      t.string :image
+      t.attachment :image
     end
   end
 
