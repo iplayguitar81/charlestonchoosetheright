b819a1370aed89dc46e1da86071e7c4af3f77034
iplayguitar81 <iplayguitar81@yahoo.com>
2015-10-31 01:21:31 -0700

    update

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,2 +1,5 @@
 class Story < ActiveRecord::Base
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 end
