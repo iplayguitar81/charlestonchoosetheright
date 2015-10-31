31a004b21c318d38e940608f0ed6af9c85e6fad5
iplayguitar81 <iplayguitar81@yahoo.com>
2015-10-31 02:10:27 -0700

    update

--- db/schema.rb
+++ db/schema.rb
@@ -592,4 +592,6 @@ ActiveRecord::Schema.define(version: 20151031081503) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  include Paperclip::Schema
+
 end
