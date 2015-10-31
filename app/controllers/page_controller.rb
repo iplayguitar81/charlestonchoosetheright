91cb686dab63e17b952eded5e46fdd795ff90fce
iplayguitar81 <iplayguitar81@yahoo.com>
2015-10-31 16:04:00 -0700

    update

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -8,6 +8,7 @@ class PageController < ApplicationController
   def home
 
     @last_recapper2 = Recapper.order("datey DESC").limit(1)
+    @last_articles = Story.order("date DESC").limit(5)
 
   end
 
