1c1a23a1ee420fe28dab7e9c7b71166d9284fedc
iplayguitar81 <iplayguitar81@yahoo.com>
2015-11-02 01:06:54 -0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Tblazersfans::Application.routes.draw do
   resources :game_recaps do
 
-    collection { post :import}
+    collection { post :import2}
 
     get 'recaps/:page', :action => :index, :on => :collection
 
