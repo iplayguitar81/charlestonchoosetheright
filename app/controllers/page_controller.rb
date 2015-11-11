ece60e697dfbb4499df8f19951a2c611d212f2a2
iplayguitar81 <iplayguitar81@yahoo.com>
2015-11-11 00:51:42 -0800

    update

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,8 +2,8 @@ class PageController < ApplicationController
 
   def contact
 
-    @name =params['name']
-    @email =params['email']
+    @name = params['name']
+    @email = params['email']
 
 
   end
