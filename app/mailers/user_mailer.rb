4e3fe924d40a3ca3c95bb11000fe4d5056553715
iplayguitar81 <iplayguitar81@yahoo.com>
2017-10-03 14:05:10 -0700

    update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,66 +4,26 @@ class UserMailer < ActionMailer::Base
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
-  #   en.user_mailer.contact_us.subject
+  #   en.user_mailer.contactus.subject
   #
+  def contactus
+    @greeting = "Hi"
 
-
-  def contact_us( admin, name, email, phone, contact, question, newsletter, song )
-    @greeting = "Howdy"
-    @name=name
-    @email=email
-    @phone=phone
-    @contact=contact
-    @question=question
-    @newsletter=newsletter
-    @song=song
-
-    mail to: "#{admin}",
-         from: "\"Ruprecht's Guitars Website Visitor Message\" <uncleruprecht@ruprechtsguitars.com>",
-         #CHANGE BELOW TO  WHOEVER ELSE OWNS SITE/READS EMAIL!!!
-         bcc: "snyder.chris.@gmail.com"
+    mail to: "to@example.org"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
-  #   en.user_mailer.thank_you.subject
+  #   en.user_mailer.newsletter.subject
   #
-  def thank_you(user)
-    @greeting = "Howdy"
+  def newsletter(game_recap, user)
+    @greeting = "Hi"
 
-    mail to: user,
-         from: "\"Ruprecht's Guitars Thanks You!\" <uncleruprecht@ruprechtsguitars.com>"
-  end
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.article_admin.subject
-  #
-  def article_admin(article_id, user)
-    @greeting = "Howdy #{user}"
-    @article = Article.find( article_id )
-
-    mail  to: user,
-          from: "\"Ruprecht's Guitars\" <uncleruprecht@ruprechtsguitars.com>",
-          #from: "uncleruprecht@ruprechtsguitars.com",
-          :subject => "New Ruprechts guitar article: #{@article.title.titleize}"
+    @game_recap = game_recap
+    mail to: user.email
 
 
-  end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.user_mailer.article_all.subject
-  #
-  def article_all(article_id, user)
-    @greeting = "Howdy"
-    @article = Article.find( article_id )
-
-    mail to: user,
-         from: "\"Ruprecht's Guitars\" <uncleruprecht@ruprechtsguitars.com>",
-         :subject => "New Ruprechts guitar article: #{@article.title.titleize}"
   end
 end
