1e0035dee47d0be7a71af5adbe89afc70d0dd2e0
iplayguitar81 <iplayguitar81@yahoo.com>
2017-10-30 20:37:47 -0700

    update

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,26 +4,67 @@ class UserMailer < ActionMailer::Base
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
-  #   en.user_mailer.contactus.subject
+  #   en.user_mailer.contact_us.subject
   #
-  def contactus
-    @greeting = "Hi"
 
-    mail to: "to@example.org"
+
+  def contact_us( admin, name, email, phone, contact, product, question, newsletter, song )
+    @greeting = "Howdy"
+    @name=name
+    @email=email
+    @phone=phone
+    @contact=contact
+    @product=product
+    @question=question
+    @newsletter=newsletter
+    @song=song
+
+    mail to: "#{admin}",
+         from: "\"TrailBlazersFans.com Visitor Message\" <admin@trailblazersfans.com>",
+         #CHANGE BELOW TO  WHOEVER ELSE OWNS SITE/READS EMAIL!!!
+         bcc: "snyder.chris.@gmail.com"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
-  #   en.user_mailer.newsletter.subject
+  #   en.user_mailer.thank_you.subject
   #
-  def newsletter(game_recap, user)
-    @greeting = "Hi"
+  def thank_you(user)
+    @greeting = "Howdy"
 
-    @game_recap = game_recap
-    mail to: user.email
+    mail to: user,
+         from: "\"Trailblazersfans.com Thanks You!\" <admin@trailblazersfans.com>"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.article_admin.subject
+  #
+  def article_admin(story_id, user)
+    @greeting = "Howdy #{user}"
+    @story = Story.find( story_id )
+
+    mail  to: user,
+          from: "\"Trailblazersfans.com\" <admin@trailblazersfans.com>",
+          #from: "admin@trailblazersfans.com",
+          :subject => "New Trailblazersfans.com article: #{@story.title.titleize}"
 
 
+  end
 
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.article_all.subject
+  #
+  def article_all(story_id, user)
+    @greeting = "Howdy"
+    @story = Story.find( story_id )
+
+    mail to: user,
+         from: "\"Trailblazersfans.com\" <admin@trailblazersfans.com>",
+         :subject => "New Trailblazersfans.com article: #{@story.title.titleize}"
   end
 end
