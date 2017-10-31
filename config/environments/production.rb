7ef97a2448d5682466b5e71c724486baba1d7471
iplayguitar81 <iplayguitar81@yahoo.com>
2017-10-30 21:24:36 -0700

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Tblazersfans::Application.configure do
   config.action_mailer.smtp_settings = {
       address:  'smtp.gmail.com',
       port: 587,
+      #domain: '',
       user_name: 'ruprechtsguitars@gmail.com',
       password: 'sizzle99',
       authentication: 'plain',
