637594cafd43f415e659ca912da2e40bffe6bcfd
iplayguitar81 <iplayguitar81@yahoo.com>
2017-11-01 17:13:36 -0700

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,18 @@ module ApplicationHelper
 
 
 
+#format date method...
+  def date_format(date)
+
+    date_time = Time.parse(date)
+    date_time.strftime("%A %B #{date_time.day.ordinalize} %Y %l:%M%P")
+
+    return date_time
+
+
+
+end
+
 
   def is_admin?
 
